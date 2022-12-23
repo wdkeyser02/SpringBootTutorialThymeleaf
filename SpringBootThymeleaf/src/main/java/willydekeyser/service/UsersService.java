80bfc240cc18492bbe46b4f8fb1af94b87c86628
willydekeyser <willy.de.keyser@skynet.be>
2022-12-23 23:15:40 +0100

    Update Lombok

--- SpringBootThymeleaf/src/main/java/willydekeyser/service/UsersService.java
+++ SpringBootThymeleaf/src/main/java/willydekeyser/service/UsersService.java
@@ -4,17 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import willydekeyser.model.Users;
 import willydekeyser.repository.UsersRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UsersService {
 	
 	private final UsersRepository usersRepository;
-
-	public UsersService(UsersRepository usersRepository) {
-		this.usersRepository = usersRepository;
-	}
 	
 	public List<Users> getUsers() {
 		return usersRepository.findAll();
